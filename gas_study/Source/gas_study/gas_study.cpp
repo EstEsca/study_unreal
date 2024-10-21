b2e1e12afcb5cf8144e81339cfebaf9c7586a1f4
Jiwon Suh <jsuh.graphic@ironmace.com>
2024-10-21 17:00:56 +0900

    gas study added

---
+++ gas_study/Source/gas_study/gas_study.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "gas_study.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, gas_study, "gas_study" );
